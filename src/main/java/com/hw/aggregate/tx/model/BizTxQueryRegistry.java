44bb5320b43c505489b2f5bb73d90b4f1b5125be
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-10-18 01:17:29 -0400

    refactor register

--- src/main/java/com/hw/aggregate/tx/model/BizTxQueryRegistry.java
+++ src/main/java/com/hw/aggregate/tx/model/BizTxQueryRegistry.java
@@ -1,22 +1,13 @@
 package com.hw.aggregate.tx.model;
 
 import com.hw.shared.sql.RestfulQueryRegistry;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 
 @Component
 public class BizTxQueryRegistry extends RestfulQueryRegistry<BizTx> {
-    @Autowired
-    private AppBizTxSelectQueryBuilder appBizTaskSelectQueryBuilder;
-    @Autowired
-    private AdminBizTxSelectQueryBuilder adminBizTaskSelectQueryBuilder;
 
     @Override
-    @PostConstruct
-    protected void configQueryBuilder() {
-        selectQueryBuilder.put(RoleEnum.ADMIN, adminBizTaskSelectQueryBuilder);
-        selectQueryBuilder.put(RoleEnum.APP, appBizTaskSelectQueryBuilder);
+    public Class<BizTx> getEntityClass() {
+        return BizTx.class;
     }
 }
