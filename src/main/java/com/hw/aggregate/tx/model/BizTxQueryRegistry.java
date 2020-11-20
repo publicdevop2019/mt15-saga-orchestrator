fc5e805c99c8f86505d2772e3467c5a9d5929210
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-11-20 00:39:26 -0500

    update shared, enable cache, pass test

--- src/main/java/com/hw/aggregate/tx/model/BizTxQueryRegistry.java
+++ src/main/java/com/hw/aggregate/tx/model/BizTxQueryRegistry.java
@@ -10,4 +10,11 @@ public class BizTxQueryRegistry extends RestfulQueryRegistry<BizTx> {
     public Class<BizTx> getEntityClass() {
         return BizTx.class;
     }
+    private void setUp() {
+        cacheable.put(RoleEnum.USER, true);
+        cacheable.put(RoleEnum.ADMIN, true);
+        cacheable.put(RoleEnum.APP, true);
+        cacheable.put(RoleEnum.PUBLIC, true);
+        cacheable.put(RoleEnum.ROOT, true);
+    }
 }
